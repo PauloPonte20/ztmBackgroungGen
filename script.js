cadb61564c571558ffded1f1ba9cc77e57ccf341
Paulo Ponte <71789122+PauloPonte20@users.noreply.github.com>
2021-07-23 16:40:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
 
-function setGradient() {
+const setGradient = () => {
     body.style.background = 
     "linear-gradient(to right, "
     + color1.value
@@ -13,6 +13,5 @@ function setGradient() {
 
     css.textContent = body.style.background + ";";
 }
-
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
